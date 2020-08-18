1015d79089bcbff2c2da1386511e901903f7dd0a
Manjunath Prabhakar <manjunath189@gmail.com>
2020-08-18 16:00:57 +0530

    added some data

--- src/main/java/unitTesting/Testing.java
+++ src/main/java/unitTesting/Testing.java
@@ -10,4 +10,8 @@ public class Testing {
     public static void display(String param){
         System.out.println("You parameter is " + param);
     }
+
+    public void someFunc(){
+        System.out.println("Testing.someFunc");
+    }
 }
